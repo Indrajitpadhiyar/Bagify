a0f8c785e63dc736f0886afcaf6cd0fb1a82673a
indrajit <indrajitpadhiyar107@gmail.com>
2025-12-29 18:40:05 +0530

    cart page error fix

--- frontend/src/components/pages/Cart.jsx
+++ frontend/src/components/pages/Cart.jsx
@@ -1,4 +1,6 @@
 import Footer from '../ui/Footer';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
     const { cartItems } = useSelector((state) => state.cart);
